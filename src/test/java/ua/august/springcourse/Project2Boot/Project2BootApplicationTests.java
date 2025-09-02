f1d7fc1896c06068a28f2d1d4c60814d9d598d6e
sleepwalker746 <sleepwalker.8@proton.me>
2025-09-02 10:00:20 +0300

    Change pom.xml

--- src/test/java/ua/august/springcourse/Project2Boot/Project2BootApplicationTests.java
+++ src/test/java/ua/august/springcourse/Project2Boot/Project2BootApplicationTests.java
@@ -1,10 +1,15 @@
 package ua.august.springcourse.Project2Boot;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import ua.august.springcourse.Project2Boot.models.Book;
+import ua.august.springcourse.Project2Boot.services.BookService;
+
 
 @SpringBootTest
 class Project2BootApplicationTests {
+
 
 	@Test
 	void contextLoads() {
